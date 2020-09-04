3a88eee8543c77ed655ec956b38cc28bddc2fa62
James Waterhouse <09jwater@gmail.com>
2020-09-04 14:06:48 +0100

    FIX: export for hover

--- src/rule/index.ts
+++ src/rule/index.ts
@@ -1,7 +1,7 @@
-export {default as media}                                             from "./MediaRule";
-export {default as rtl}                                               from "./RtlRule";
-export {default as platform}                                          from "./PlatformRule";
-export {default as pseudo, createPseudoRule, active, focus, disabled} from "./PseudoRule";
-export {default as child}                                             from "./ChildRule";
+export {default as media}                                                    from "./MediaRule";
+export {default as rtl}                                                      from "./RtlRule";
+export {default as platform}                                                 from "./PlatformRule";
+export {default as pseudo, createPseudoRule, active, focus, disabled, hover} from "./PseudoRule";
+export {default as child}                                                    from "./ChildRule";
 
 export {and, or, not, createStyleRule} from "./StyleRule";
